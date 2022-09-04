0cde01fde6ef3b259a4919c7bba71419575b7f75
Vitoriaalic3 <vitoriaalic3@gmail.com>
2022-09-04 18:24:44 -0300

    1

--- Modelagem de dados/UML Java/Classes/src/DAO/AgenciaDAO.java
+++ Modelagem de dados/UML Java/Classes/src/DAO/AgenciaDAO.java
@@ -1,5 +1,42 @@
 package DAO;
 
-public class AgenciaDAO {
 
-}
+	import java.sql.Connection;
+	import java.sql.Date;
+	import java.sql.PreparedStatement;
+	import java.sql.ResultSet;
+	import java.util.ArrayList;
+	import java.util.List;
+	
+	public class AgenciaDAO {
+		public void save(Agencia agencia) {
+			String sql="INSERT INTO Agenda(nome, endereco)" + 
+		"VALUES (?,?)";
+			
+			Connection conn=null;
+			PreparedStatement pstm=null;
+			
+			try {
+				conn=ConnectionFactory.creatConnectiontoMySQL();
+				
+				pstm=(PreparedStatement) conn.prepareStatement(sql);
+				pstm.setString(1, agencia.getNome());
+				pstm.setString(2, agencia.getEndereco());
+				
+				pstm.execute();
+			}catch (Exception e) {
+				e.printStackTrace();
+			}finally {
+				try{
+					if(pstm!=null) {
+						pstm.close();
+				}
+					if(conn!=null) {
+						conn.close();
+					}
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+			}
+		}
+	}
